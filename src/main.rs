743db098ce62d83020d749fb69d12def74003aa2
Tyjfre-j <bouhamzaabouda@gmail.com>
2026-09-11 06:39:09 +0100

    feat: basic axum server with /health route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+async fn handle_health() -> &'static str {
+    "OK"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health", get(handle_health));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
