3a3b030eac89b6d9f86180b5ecf211ed6d91c486
Tyjfre-j <bouhamzaabouda@gmail.com>
2026-09-23 09:37:32 +0100

    Re-export shared validation constants and has_control_characters from protocol crate

--- client/src/protocol.rs
+++ client/src/protocol.rs
@@ -1,4 +1,7 @@
-pub use ws_chat_protocol::{ClientMessage, ErrorCode, ServerMessage};
+pub use ws_chat_protocol::{
+    ClientMessage, ErrorCode, MAX_CHAT_MESSAGE_LEN, MAX_ROOM_NAME_LEN, MAX_USERNAME_LEN, Received,
+    ServerMessage, has_control_characters,
+};
 
 #[derive(Default)]
 pub enum ClientStage {
